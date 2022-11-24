34088536c7f0dd3a2ea8e0b4c967dae4374fa105
gasiorMateusz <mateuszgasior314@gmail.com>
2022-11-24 17:00:01 +0100

    place maps in correct order

--- src/main/java/com/codecool/dungeoncrawl/logic/MultiMap.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MultiMap.java
@@ -2,14 +2,14 @@ package com.codecool.dungeoncrawl.logic;
 
 public class MultiMap {
 
-    private final String[] maps = {"/death.txt","/ghost.txt","/crocodile.txt","/octopus.txt","/items.txt","/DEMO-longmap.txt", "/doors.txt"};
+    private final String[] maps = {"/death.txt","/ghost.txt","/crocodile.txt","/octopus.txt","/items.txt","/doors.txt","/DEMO-longmap.txt"};
 
     public MultiMap() {
     }
 
     public String getMapFromSet(int index) {
         if ((index > -1) && (index < this.maps.length)) {
-            return maps[index];
+            return maps[maps.length-1-index];
         } else {
             return this.maps[0];
         }
