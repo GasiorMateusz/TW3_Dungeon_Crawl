91424d99c01cb460c87c1555a29eb78b0e36331a
Marcin <marcin.szuwalski@motorolasolutions.com>
2022-12-05 22:36:36 +0100

    Added a subtype field to differentiate between medical, weapon and game items

--- src/main/java/com/codecool/dungeoncrawl/logic/items/ItemType.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/ItemType.java
@@ -1,21 +1,26 @@
 package com.codecool.dungeoncrawl.logic.items;
 
 public enum ItemType {
-    CROWN('d'),
-    KEY('K'),
-    SWORD('W'),
-    BOW('B'),
-    NEWLIFE('H'),
-    MEDICINE('M'),
-    BANDAGE('D');
+    CROWN('d', "Game"),
+    KEY('K',"Game"),
+    SWORD('W', "Weapon"),
+    BOW('B', "Weapon"),
+    NEWLIFE('H', "Game"),
+    MEDICINE('M', "Medical"),
+    BANDAGE('D', "Medical");
 
     private final char mapChar;
+    private final String subtype;
 
-    ItemType(char mapChar) {
+    ItemType(char mapChar, String subtype) {
         this.mapChar = mapChar;
+        this.subtype = subtype;
     }
 
     public static char getItemMapChar(ItemType item) {
         return item.mapChar;
+    }
+    public static String getItemSubtype(ItemType item) {
+        return item.subtype;
     }
 }
