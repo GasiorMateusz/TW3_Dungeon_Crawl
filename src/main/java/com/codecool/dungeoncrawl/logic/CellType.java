ea5883b404754fd8a002ecd190b6d64296299030
Marcin <marcin.szuwalski@motorolasolutions.com>
2022-12-05 20:46:44 +0100

    Extended CellType enum with char mapChar for dealing with cells in cleaner code

--- src/main/java/com/codecool/dungeoncrawl/logic/CellType.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/CellType.java
@@ -2,20 +2,26 @@ package com.codecool.dungeoncrawl.logic;
 
 public enum CellType {
 
-    EMPTY("empty"),
-    FLOOR("floor"),
-    WALL("wall"),
-    CLOSED_DOOR("closedDoor"),
-    OPEN_DOOR("openDoor"),
-    STAIRS("stairs");
+    EMPTY("empty", ' '),
+    FLOOR("floor", '.'),
+    WALL("wall",'#'),
+    CLOSED_DOOR("closedDoor", 'C'),
+    OPEN_DOOR("openDoor",'O'),
+    STAIRS("stairs", 'X');
 
     private final String tileName;
+    private final char mapChar;
 
-    CellType(String tileName) {
+    CellType(String tileName, char mapChar) {
         this.tileName = tileName;
+        this.mapChar = mapChar;
     }
 
     public String getTileName() {
         return tileName;
+    }
+
+    public char getMapChar() {
+        return mapChar;
     }
 }
