93f9b4e9889f20e6b5e65a4bbe35c711623a9d67
MikolajczykDominika <mikolajczyk.dominika94@gmail.com>
2022-12-08 13:04:31 +0100

    Added FileLoader

--- src/main/java/com/codecool/dungeoncrawl/json/FileLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/json/FileLoader.java
@@ -1,12 +1,18 @@
 package com.codecool.dungeoncrawl.json;
 
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileLoader {
+    String content;
     public String loadFromFile(String filename){
-        //Path filePath = Path.of(filename);
-        //String content = readUsingBufferedReader(filePath);
-        return "cos";
+        try {
+            content = Files.readString(Paths.get(filename));
+        }catch (Exception IOException){
+            System.out.println("Can't load the data");
+        }
+        return content;
     }
 }
