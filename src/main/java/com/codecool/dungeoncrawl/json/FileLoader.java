0fc7e3a754721ac452bfb6c30124b88834cc7000
MikolajczykDominika <mikolajczyk.dominika94@gmail.com>
2022-12-08 11:41:29 +0100

    FileLoader implementation

--- src/main/java/com/codecool/dungeoncrawl/json/FileLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/json/FileLoader.java
@@ -1,7 +1,12 @@
 package com.codecool.dungeoncrawl.json;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileLoader {
     public String loadFromFile(String filename){
-        return "fileAsString";
+        //Path filePath = Path.of(filename);
+        //String content = readUsingBufferedReader(filePath);
+        return "cos";
     }
 }
